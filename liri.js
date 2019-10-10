13f16efb95a1d0dcb6dce2d7397c32403fce0bd1
Kala Elam <speechgirl1505@gmail.com>
2019-10-10 13:06:21 -0500

    learning new things

--- liri.js
+++ liri.js
@@ -1,3 +1,18 @@
+// * `concert-this`
+// node liri.js concert-this <artist/band name here>
+"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+// * `spotify-this-song`
+// node liri.js spotify-this-song '<song name here>'
+
+
+// * `movie-this`
+// node liri.js movie-this '<movie name here>'
+
+
+// * `do-what-it-says`
+// node liri.js do-what-it-says
+
 require("dotenv").config();
 
 var keys = require("./keys.js");
