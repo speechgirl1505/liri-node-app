ef27d5b7f9acb3f057d0a7f16cab31fe7d0d42e8
Kala Elam <speechgirl1505@gmail.com>
2019-10-14 11:33:59 -0500

    added for each loop on concerts

--- README.md
+++ README.md
@@ -10,20 +10,19 @@ https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
 
 4) Technologies used/Built with
 
-
-
-
 Liri Node App
     In this app we are calling on three different APIs for specific data using Node.js. The API's being targeted are Spotify, Bands in Town, and OMDB. These will pull information for either songs, concerts, or movies inputed by the user and give specific information back. For concerts you will get back the name of the venue, location of the venue, and date of the event in (MM/DD/YYY) format. For songs you will get the Artist (s), song name, a preview link for the song, and the Album the song is on. For the movie you will get the Title, Release Year, IMDB Ratings, Rotten Tomatoes Ratings, Country produced, Lanuage, Plot, and Actors. 
 
 Getting Started
-    --How to install if you clone the repo
+    How to install if you clone the repo
     you need to npm init in the root folder
     you need to sign up and get spotify api keys
     create a .env file and .gitignore file and put x information in there
             SPOTIFY_SECRET=process.env. 
             SPOTIFY_ID=your-spotify-id
             SPOTIFY_SECRET=your-spotify-secret
+
+Screen Shots/Examples
 
 
 Built With
@@ -36,9 +35,6 @@ Built With
     OMDB - Movie API
     Stress - state of mental or emotional strain or tension resulting from adverse or very demanding circumstances.
     Bands In Town - Concert API
-
-Authors 
-    Kala Elam
 
 
 Acknowledgments
--- liri.js
+++ liri.js
@@ -9,74 +9,93 @@ var spotify = new Spotify(keys.spotify);
 
 var fs = require("fs");
 
-
 // //THIS IS BANDS======================================================================================================================
-// concert-this function 
+// concert-this function
 function itsShowTime(artistName) {
-    var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
-    axios.get(queryUrl).then(
-    function(response) {
-    var showTime = moment(response.data[0].datetime).format("MM/DD/YYYY")
-    console.log(`
-    Name of Venue: ${response.data[0].venue.name}
-    Venue Location: ${response.data[0].venue.city}, ${response.data[0].venue.region}
-    Date of Event: ${showTime}
-    `);
-
-    // response.data.forEach(function(response) {
-    //   var showTime = moment(response.datetime).format("MM/DD/YYYY");
-    //   console.log(
-    //     `
-    //     Name of Venue: ${response.venue.name}
-    //     Venue Location: ${response.venue.city}, ${response.venue.region}
-    //     Date of Event: ${response.showTime}
-    //     `
-    //     );
+  var queryUrl =
+    "https://rest.bandsintown.com/artists/" +
+    artistName +
+    "/events?app_id=codingbootcamp";
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      response.data.forEach(function(response) {
+        // console.log(response);
+        var showTime = moment(response.datetime).format("MM/DD/YYYY");
+        console.log(
+          `
+        Name of Venue: ${response.venue.name}
+        Venue Location: ${response.venue.city}, ${response.venue.region}
+        Date of Event: ${showTime}
+        `
+        );
+      });
     })
     .catch(function(error) {
       console.log(error);
     });
-};
+
+  // var showTime = moment(response.data[0].datetime).format("MM/DD/YYYY");
+  // var showTime1 = moment(response.data[1].datetime).format("MM/DD/YYYY");
+  // var showTime2 = moment(response.data[2].datetime).format("MM/DD/YYYY");
+  // console.log(
+  //   `
+  // Name of Venue: ${response.data[0].venue.name}
+  // Venue Location: ${response.data[0].venue.city}, ${response.data[0].venue.region}
+  // Date of Event: ${showTime}
+
+  // Name of Venue: ${response.data[1].venue.name}
+  // Venue Location: ${response.data[1].venue.city}, ${response.data[1].venue.region}
+  // Date of Event: ${showTime1}
+
+  // Name of Venue: ${response.data[2].venue.name}
+  // Venue Location: ${response.data[2].venue.city}, ${response.data[2].venue.region}
+  // Date of Event: ${showTime2}
+  // `
+  // );
+
+}
 // //THIS IS SPOTIFY====================================================================================================================
-// // spotify-this-song function 
+// // spotify-this-song function
 function playThatFunkyMusic(songName) {
   spotify
-  .search({ type: 'track', query: songName})
-  .then(function(response) {
-
-    response.tracks.items.forEach(function(song) {
-      console.log(
-        `
+    .search({ type: "track", query: songName })
+    .then(function(response) {
+      response.tracks.items.forEach(function(song) {
+        console.log(
+          `
         Artist(s): ${song.album.artists[0].name}
         Song Name: ${song.name}
         Preview Link: ${song.preview_url}
         Album: ${song.album.name}
         `
         );
-    });
+      });
 
-    // console.log(
-    //   `
-    //   Artist(s): ${response.tracks.items[0].album.artists[0].name}
-    //   Song Name: ${response.tracks.items[0].name}
-    //   Preview Link: ${response.tracks.items[0].preview_url}
-    //   Album: ${response.tracks.items[0].album.name}
-    //   `
-    //   );
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+      // console.log(
+      //   `
+      //   Artist(s): ${response.tracks.items[0].album.artists[0].name}
+      //   Song Name: ${response.tracks.items[0].name}
+      //   Preview Link: ${response.tracks.items[0].preview_url}
+      //   Album: ${response.tracks.items[0].album.name}
+      //   `
+      //   );
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 //THIS IS OMDB=======================================================================================================================
-// movie-this function 
+// movie-this function
 function action(movieName) {
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-axios.get(queryUrl).then(
-  function(response) {
-    console.log(
-      `
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      console.log(
+        `
       Title: ${response.data.Title}
       Release Year: ${response.data.Year}
       IMDB Ratings: ${response.data.imdbRating}
@@ -87,57 +106,54 @@ axios.get(queryUrl).then(
       Actors: ${response.data.Actors}
       `
       );
-    
-
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
-
-};
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
 // THIS IS do-what-it-says=========================================================================================================
 // do-what-it-says function
-function doIt(){
-console.log('do what it says is called')
-fs.readFile("random.txt", "utf8", function(error, data) {
-
+function doIt() {
+  console.log("do what it says is called");
+  fs.readFile("random.txt", "utf8", function(error, data) {
     if (error) {
       return console.log(error);
     }
     var dataArr = data.split(",");
     console.log(dataArr);
-    doWhatISay(dataArr[0],dataArr[1]);
-});
+    doWhatISay(dataArr[0], dataArr[1]);
+  });
 }
 
-function doWhatISay(method,item){
-switch (method) {
+function doWhatISay(method, item) {
+  switch (method) {
     case "concert-this":
       itsShowTime(item);
       break;
-    
+
     case "spotify-this-song":
       playThatFunkyMusic(item);
       break;
-    
+
     case "movie-this":
-      action(item)
+      action(item);
       break;
-    
+
     case "do-what-it-says":
       doIt();
       break;
-    
-      default: console.log(`
+
+    default:
+      console.log(`
       Please run one of the following commands:
       
       concert-this
       spotify-this
       movie-this
       do-what-it-says
-      `)
-    }
-};
+      `);
+  }
+}
 
 var whatDoYouWant = process.argv.slice(3).join(" ");
-doWhatISay(process.argv[2],whatDoYouWant);
+doWhatISay(process.argv[2], whatDoYouWant);
